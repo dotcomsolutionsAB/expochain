1a96042cb5e84b8b6237acf1f16113b3406a0df2
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2024-10-18 14:27:49 +0530

    update for communication tables

---
+++ database/migrations/2024_10_18_085418_create_t_email_queue_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_email_queue', function (Blueprint $table) {
+            $table->id();
+            $table->integer('jamiat_id');
+            $table->integer('family_id');
+            $table->string('to');
+            $table->string('cc')->nullable();
+            $table->string('bcc')->nullable();
+            $table->string('from');
+            $table->string('subject');
+            // as it don't support `length`, it can store upto `65,535 characters for TEXT type in MySQL`
+            $table->text('content');
+            $table->string('attachment')->nullable();
+            $table->text('response')->nullable();
+            $table->enum('status', ['pending', 'sent', 'failed']);
+            $table->string('log_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_email_queue');
+    }
+};
