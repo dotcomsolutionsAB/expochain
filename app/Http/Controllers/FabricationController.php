be49865d6fe13b5273523f47bd965a5f47d2284c
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2024-10-22 14:45:44 +0530

    update or fabrication

--- app/Http/Controllers/FabricationController.php
+++ app/Http/Controllers/FabricationController.php
@@ -43,4 +43,61 @@ class FabricationController extends Controller
         ? response()->json(['Fabrication registered successfully!', 'data' => $register_fabrication], 201)
         : response()->json(['Failed to register Fabrication record'], 400);
     }
+
+    public function fabrication()
+    {        
+        $get_fabrication = FabricationModel::select('id', 'fabrication_date','product_id', 'product_name','type', 'quantity', 'godown', 'rate', 'amount', 'description', 'log_user')->get();
+        
+
+        return isset($get_fabrication) && $get_fabrication !== null
+        ? response()->json(['Fetch data successfully!', 'data' => $get_fabrication], 200)
+        : response()->json(['Failed to fetch data'], 404); 
+    }
+
+    // update
+    public function edit_fabrication(Request $request, $id)
+    {
+        $request->validate([
+            'fabrication_date' => 'required|date',
+            'product_id' => 'required|integer',
+            'product_name' => 'required|string',
+            'type' => 'required|in:wastage,sample',
+            'quantity' => 'required|integer',
+            'godown' => 'required|integer',
+            'rate' => 'required|numeric',
+            'amount' => 'required|numeric',
+            'description' => 'nullable|string',
+            'log_user' => 'required|string'
+        ]);
+
+        $update_fabrication = FabricationModel::where('id', $id)
+        ->update([
+            'fabrication_date' => $request->input('fabrication_date'),
+            'product_id' => $request->input('product_id'),
+            'product_name' => $request->input('product_name'),
+            'type' => $request->input('type'),
+            'quantity' => $request->input('quantity'),
+            'godown' => $request->input('godown'),
+            'rate' => $request->input('rate'),
+            'amount' => $request->input('amount'),
+            'description' => $request->input('description'),
+            'log_user' => $request->input('log_user')
+        ]);
+        
+        return $update_fabrication
+        ? response()->json(['Fabrication updated successfully!', 'data' => $update_fabrication], 201)
+        : response()->json(['No changes detected'], 204);
+    }
+
+    // delete
+    public function delete_fabrication($id)
+    {
+        // Delete the fabrication
+        $delete_fabrication = FabricationModel::where('id', $id)->delete();
+
+        // Return success response if deletion was successful
+        return $delete_fabrication
+        ? response()->json(['message' => 'Delete Fabrication successfully!'], 204)
+        : response()->json(['message' => 'Sorry, Fabrication not found'], 400);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -58,87 +58,87 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/godown', [MastersController::class, 'add_godown']);
     Route::get('/godown', [MastersController::class, 'view_godown']);
     Route::post('/update_godown/{id?}', [MastersController::class, 'edit_godown']);
-    Route::delete('/godown', [MastersController::class, 'delete_godown']);
+    Route::delete('/godown/{id?}', [MastersController::class, 'delete_godown']);
 
     Route::post('/category', [MastersController::class, 'add_category']);
     Route::get('/pdf', [MastersController::class, 'view_category']);
     Route::post('/update_category/{id?}', [MastersController::class, 'edit_category']);
-    Route::delete('/pdf', [MastersController::class, 'delete_category']);
+    Route::delete('/pdf/{id?}', [MastersController::class, 'delete_category']);
 
     Route::post('/sub_category', [MastersController::class, 'add_sub_category']);
     Route::get('/pdf', [MastersController::class, 'sub_category']);
     Route::post('/update_pdf/{id?}', [MastersController::class, 'edit_sub_category']);
-    Route::delete('/pdf', [MastersController::class, 'delete_sub_category']);
+    Route::delete('/pdf/{id?}', [MastersController::class, 'delete_sub_category']);
 
     Route::post('/brand', [MastersController::class, 'add_brand']);
     Route::get('/brand', [MastersController::class, 'brand']);
     Route::post('/update_brand/{id?}', [MastersController::class, 'edit_brand']);
-    Route::delete('/brand', [MastersController::class, 'delete_brand']);
+    Route::delete('/brand/{id?}', [MastersController::class, 'delete_brand']);
 
     Route::post('/add_quotations', [QuotationsController::class, 'add_quotations']);
     Route::get('/quotations', [QuotationsController::class, 'quotations']);
     Route::post('/update_quotations/{id?}', [MastersController::class, 'edit_quotations']);
-    Route::delete('/quotations', [MastersController::class, 'delete_quotations']);
+    Route::delete('/quotations/{id?}', [MastersController::class, 'delete_quotations']);
 
     Route::post('/add_sales', [SalesOrderController::class, 'add_sales_order']);
     Route::get('/sales', [SalesOrderController::class, 'pdf_template']);
     Route::post('/update_sales/{id?}', [SalesOrderController::class, 'edit_pdf_template']);
-    Route::delete('/sales', [SalesOrderController::class, 'delete_pdf_template']);
+    Route::delete('/sales/{id?}', [SalesOrderController::class, 'delete_pdf_template']);
 
     Route::post('/add_sales_invoice', [SalesInvoiceController::class, 'add_sales_invoice']);
     Route::get('/sales', [SalesOrderController::class, 'pdf_template']);
     Route::post('/update_sales/{id?}', [SalesOrderController::class, 'edit_pdf_template']);
-    Route::delete('/sales', [SalesOrderController::class, 'delete_pdf_template']);
+    Route::delete('/sales/{id?}', [SalesOrderController::class, 'delete_pdf_template']);
 
     Route::post('/add_credit_note', [CreditNoteController::class, 'add_credit_note']);
     Route::get('/credit_note', [CreditNoteController::class, 'pdf_template']);
     Route::post('/update_credit_note/{id?}', [CreditNoteController::class, 'edit_credit_note']);
-    Route::delete('/credit_note', [CreditNoteController::class, 'delete_credit_note']);
+    Route::delete('/credit_note/{id?}', [CreditNoteController::class, 'delete_credit_note']);
 
     Route::post('/add_test_certificate', [TestCertificateController::class, 'add_test_certificate']);
     Route::get('/test_certificate', [TestCertificateController::class, 'test_certificate']);
     Route::post('/update_test_certificate/{id?}', [TestCertificateController::class, 'edit_test_certificate']);
-    Route::delete('/test_certificate', [TestCertificateController::class, 'delete_test_certificate']);
+    Route::delete('/test_certificate/{id?}', [TestCertificateController::class, 'delete_test_certificate']);
 
     Route::post('/add_purchase_invoice', [PurchaseInvoiceController::class, 'add_purchase_invoice']);
     Route::get('/purchase_invoice', [PurchaseInvoiceController::class, 'purchase_invoice']);
     Route::post('/update_purchase_invoice/{id?}', [PurchaseInvoiceController::class, 'edit_purchase_invoice']);
-    Route::delete('/purchase_invoice', [PurchaseInvoiceController::class, 'delete_purchase_invoice']);
+    Route::delete('/purchase_invoice/{id?}', [PurchaseInvoiceController::class, 'delete_purchase_invoice']);
 
     Route::post('/add_purchase_order', [PurchaseOrderController::class, 'add_purchase_order']);
     Route::get('/purchase_order', [PurchaseOrderController::class, 'purchase_order']);
     Route::post('/update_purchase_order/{id?}', [PurchaseOrderController::class, 'edit_purchase_order']);
-    Route::delete('/purchase_order', [PurchaseOrderController::class, 'delete_purchase_order']);
+    Route::delete('/purchase_order/{id?}', [PurchaseOrderController::class, 'delete_purchase_order']);
 
     Route::post('/add_purchase_return', [PurchaseReturnController::class, 'add_purchase_return']);
     Route::get('/purchase_return', [PurchaseReturnController::class, 'purchase_return']);
     Route::post('/update_purchase_return/{id?}', [PurchaseReturnController::class, 'edit_purchase_return']);
-    Route::delete('/purchase_return', [PurchaseReturnController::class, 'delete_purchase_return']);
+    Route::delete('/purchase_return/{id?}', [PurchaseReturnController::class, 'delete_purchase_return']);
 
     Route::post('/add_debit_note', [DebitNoteController::class, 'add_debit_note']);
     Route::get('/debit_note', [DebitNoteController::class, 'debit_note']);
     Route::post('/update_debit_note/{id?}', [DebitNoteController::class, 'edit_debit_note']);
-    Route::delete('/debit_note', [DebitNoteController::class, 'delete_debit_note']);
+    Route::delete('/debit_note/{id?}', [DebitNoteController::class, 'delete_debit_note']);
 
     Route::post('/add_stock_transfer', [StockTransferController::class, 'add_stock_transfer']);
     Route::get('/tock_transfer', [StockTransferController::class, 'stock_transfer']);
     Route::post('/update_stock_transfer/{id?}', [StockTransferController::class, 'edit_tock_transfer']);
-    Route::delete('/stock_transfer', [StockTransferController::class, 'delete_tock_transfer']);
+    Route::delete('/stock_transfer/{id?}', [StockTransferController::class, 'delete_tock_transfer']);
 
     Route::post('/add_assembly', [AssemblyController::class, 'add_assembly']);
     Route::get('/assembly', [AssemblyController::class, 'assembly']);
     Route::post('/update_assembly/{id?}', [AssemblyController::class, 'edit_assembly']);
-    Route::delete('/assembly', [AssemblyController::class, 'delete_assembly']);
+    Route::delete('/assembly/{id?}', [AssemblyController::class, 'delete_assembly']);
 
     Route::post('/add_assembly_operations', [AssemblyOperationsController::class, 'add_assembly_operations']);
     Route::get('/assembly_operations', [AssemblyOperationsController::class, 'assembly_operations']);
     Route::post('/update_assembly_operations/{id?}', [AssemblyOperationsController::class, 'edit_assembly_operations']);
-    Route::delete('/assembly_operations', [AssemblyOperationsController::class, 'delete_assembly_operations']);
+    Route::delete('/assembly_operations/{id?}', [AssemblyOperationsController::class, 'delete_assembly_operations']);
 
     Route::post('/add_fabrication', [FabricationController::class, 'add_fabrication']);
     Route::get('/fabrication', [FabricationController::class, 'fabrication']);
     Route::post('/update_fabrication/{id?}', [FabricationController::class, 'edit_fabrication']);
-    Route::delete('/fabrication', [FabricationController::class, 'delete_fabrication']);
+    Route::delete('/fabrication/{id?}', [FabricationController::class, 'delete_fabrication']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
 });
