7b7c69f9344f95891837a90be0fb829f1776744b
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2024-11-20 15:16:22 +0530

    update for tables

--- database/migrations/2024_11_20_063033_create_t_opening_stock_table.php
+++ database/migrations/2024_11_20_063033_create_t_opening_stock_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->integer('company_id');
             $table->integer('year');
             $table->integer('godown_id');
+            $table->integer('product_id');
             $table->integer('quantity');
             $table->float('value');
             $table->integer('sold');
