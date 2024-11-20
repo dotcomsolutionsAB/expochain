ca79ccc1a477ce0f5b63c6b0608a3a7d936dc284
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2024-11-20 12:04:11 +0530

    make 3 `migrate tables`

---
+++ database/migrations/2024_11_20_063105_create_t_closing_stock_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_closing_stock', function (Blueprint $table) {
+            $table->id();
+            $table->integer('company_id');
+            $table->integer('year');
+            $table->integer('godown_id');
+            $table->integer('quantity');
+            $table->float('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_closing_stock');
+    }
+};
