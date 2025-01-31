33645a624a8ba4b65bda8ae061151cebcf35f50e
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-01-31 12:30:23 +0530

    add date as per requirements

--- app/Http/Controllers/QuotationsController.php
+++ app/Http/Controllers/QuotationsController.php
@@ -239,7 +239,7 @@ class QuotationsController extends Controller
                     $query->select('id', 'name');
             },
         ])
-        ->select('id', 'client_id', 'client_contact_id', 'name', 'address_line_1', 'address_line_2', 'city', 'pincode', 'state', 'country', 'quotation_no', 'quotation_date', 'status', 'user', 'enquiry_no', 'enquiry_date', 'sales_person', 'sales_contact', 'sales_email', 'discount', 'cgst', 'sgst', 'igst', 'total', 'currency', 'template')
+        ->select('id', 'client_id', 'client_contact_id', 'name', 'address_line_1', 'address_line_2', 'city', 'pincode', 'state', 'country', 'quotation_no', 'quotation_date', 'status', 'user', 'enquiry_no', 'enquiry_date', 'sales_person', 'sales_contact', 'sales_email', 'discount', 'cgst', 'sgst', 'igst', 'total', 'currency', 'template', DB::raw('DATE(created_at) as date'))
         ->where('company_id', Auth::user()->company_id);
 
         // Apply filters
