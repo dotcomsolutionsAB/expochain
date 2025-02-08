3b6da10cb1d91e1e74ec8d8524271b58795f0c72
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-02-08 16:14:21 +0530

    test

--- app/Http/Controllers/SalesInvoiceController.php
+++ app/Http/Controllers/SalesInvoiceController.php
@@ -808,8 +808,6 @@ class SalesInvoiceController extends Controller
 
     public function importSalesInvoices()
     {
-        dd(Auth::user());
-
         set_time_limit(1200);
 
         // Clear existing data from SalesInvoice and related tables
