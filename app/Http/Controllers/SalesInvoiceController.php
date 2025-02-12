6f2c3cd26cf7700efb359409deee38f8cc7898da
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-02-12 17:09:28 +0530

    test

--- app/Http/Controllers/SalesInvoiceController.php
+++ app/Http/Controllers/SalesInvoiceController.php
@@ -601,7 +601,7 @@ class SalesInvoiceController extends Controller
                     'igst' => $taxData['igst'] ?? 0,
                     'total' => $record['total'] ?? 0,
                     'template' => json_decode($record['pdf_template'], true)['id'] ?? '0',
-                    'commission' => !empty($record['commission']) ? (float) $record['commission'] : 0,
+                    // 'commission' => !empty($record['commission']) ? (float) $record['commission'] : 0,
                     'cash' => !empty($record['cash']) ? (string) $record['cash'] : '0',
                     'created_at' => now(),
                     'updated_at' => now()
