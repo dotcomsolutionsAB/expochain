9412c5da53c1dfc60e8f3423c453b4ac5d31b58b
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-02-20 12:32:32 +0530

    test and update migrate files

--- database/migrations/2024_10_17_090550_create_t_quotations_table.php
+++ database/migrations/2024_10_17_090550_create_t_quotations_table.php
@@ -30,8 +30,8 @@ return new class extends Migration
             $table->float('igst');
             $table->float('total');
             $table->string('currency');
-            $table->float('gross');
-            $table->float('round_off');
+            $table->float('gross')->nullable();
+            $table->float('round_off')->nullable();
             $table->timestamps();
         });
     }
