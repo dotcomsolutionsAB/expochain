ad5d33327a43e190ced54b917116b4cdfafc9236
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-03-24 14:01:04 +0530

    test for clean laravel log

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clear-log', function () {
+    file_put_contents(storage_path('logs/laravel.log'), '');
+    return 'Laravel log cleared!';
+});
+
