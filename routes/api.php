3d324c59dffd4d5af71fa1a9673297396229b3fb
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2025-05-31 16:08:23 +0530

    ..

--- routes/api.php
+++ routes/api.php
@@ -49,7 +49,7 @@ Route::post('/get_otp', [AuthController::class, 'generate_otp']);
 Route::middleware(['auth:sanctum'])->group(function () {
 
     // Route::get('/reset_calculation/{id}', [ResetController::class, 'stock_calculation']);
-    // Route::get('/reset_calculation', [ResetController::class, 'stock_calculation']);
+    Route::get('/reset_calculation', [ResetController::class, 'stock_calculation']);
 
 
     Route::get('/user', [UsersController::class, 'view']);
