e7aab26681c6241d25dce54d2673e73a7a73c8a9
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-09-06 15:53:23 +0530

    update and fix

--- app/Http/Controllers/SuppliersController.php
+++ app/Http/Controllers/SuppliersController.php
@@ -15,8 +15,9 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Illuminate\Support\Facades\Storage;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use Maatwebsite\Excel\Concerns\WithStyles;
-use Maatwebsite\Excel\Style\Border;
-use Maatwebsite\Excel\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;  // Correct import for Alignment class
+use Maatwebsite\Excel\Style\Border;  // Border class import is fine
+
 
 class SuppliersController extends Controller
 {
